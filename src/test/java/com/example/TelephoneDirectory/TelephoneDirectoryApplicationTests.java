87defa8797aa57bb3634caa11a771eb16356fd44
CurlyWendy <bubblegumsharkie@gmail.com>
2024-03-19 12:52:25 +0500

    Создан проект

---
+++ src/test/java/com/example/TelephoneDirectory/TelephoneDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TelephoneDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelephoneDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
